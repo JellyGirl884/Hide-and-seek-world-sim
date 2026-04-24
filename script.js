0b800f882ad2b6dd20ab1f131e62bcf39134dd8b
JellyGirl884 <rohun.mustunderstand@gmail.com>
2026-04-24 20:12:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const map = L.map('map').setView([20, 0], 2);
 
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+}).addTo(map);
+
+map.on('click', function(e) {
+  L.circle(e.latlng, {
+    radius: 100000,
+    color: 'red',
+    fillColor: 'red',
+    fillOpacity: 0.3
+  }).addTo(map);
+});
