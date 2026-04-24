50d12bc33249e9166735d76ecd24db54a789e8e2
JellyGirl884 <rohun.mustunderstand@gmail.com>
2026-04-24 20:26:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
-const map = L.map('map').setView([20, 0], 2);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19,
-}).addTo(map);
+window.onload = function () {
+  const map = L.map('map').setView([20, 0], 2);
 
-map.on('click', function(e) {
-  L.circle(e.latlng, {
-    radius: 100000,
-    color: 'red',
-    fillColor: 'red',
-    fillOpacity: 0.3
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
   }).addTo(map);
-});
+
+  alert("Map loaded successfully!");
+};
